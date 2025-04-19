e65e424cca3388ddb34e3f0f8676878b7c14c18a
DoDuckAn <doducan13200401@gmail.com>
2025-04-19 10:02:07 +0700

    4.DataTable + get Api

--- src/components/CustomerTable.jsx
+++ src/components/CustomerTable.jsx
@@ -2,32 +2,24 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';                  
 import 'primeicons/primeicons.css';  
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button';
 import { GoPencil } from 'react-icons/go';
 import version from '../assets/version.png';
-import { classNames } from 'primereact/utils';
 
 const CustomerTable = () => {
-    const products = [
-        {
-            id: 1,
-            selected: false,
-            name: 'Eliz',
-            company: 'AvatarSystems',
-            orderValue: '$539',
-            orderDate: '10/07/2024',
-            status: 'New',
-        }
-    ];
-
-    // ✅ Cột checkbox đầu tiên
-    const checkboxTemplate = (rowData, options) => {
-        return <Checkbox onChange={e => console.log('checkbox changed')} checked={rowData.selected}></Checkbox>;
-    };
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        fetch('https://67cd6670dd7651e464ee43e8.mockapi.io/customer')
+        .then(res=>res.json())
+        .then(data=>{
+            setData(data)
+        })
+        .catch(err=>console.log(err))
+    },[])
 
     // ✅ Avatar + Tên
     const customerTemplate = (rowData) => {
@@ -41,8 +33,26 @@ const CustomerTable = () => {
 
     // ✅ Trạng thái có màu nền
     const statusTemplate = (rowData) => {
+        let textColor
+        let bgColor
+        switch (rowData.status) {
+            case 'New':
+                bgColor='bg-green-300'
+                textColor='text-green-500'
+                break;
+            case 'In progress':
+                bgColor='bg-orange-300'
+                textColor='text-orange-500'
+                break;
+            case 'Completed':
+                bgColor='bg-blue-300'
+                textColor='text-blue-600'
+                break;
+            default:
+                break;
+        }
         return (
-            <div className="px-2 py-1 text-green-400 bg-green-200 rounded-md text-center w-fit">
+            <div className={`px-2 py-1 ${textColor} ${bgColor} rounded-md text-center w-fit`}>
                 {rowData.status}
             </div>
         );
@@ -58,14 +68,14 @@ const CustomerTable = () => {
     };
 
     return (
-        <div className="card">
-            <DataTable value={products} tableStyle={{ minWidth: '50rem'}} className='w-full'>
-                <Column body={checkboxTemplate} style={{ width: '3rem' }} />
+        <div className="card w-full">
+            <DataTable value={data} tableStyle={{ minWidth: '50rem'}} className='w-full'>
+                <Column style={{ width: '3rem' }} />
                 <Column header="CUSTOMER NAME" body={customerTemplate} />
                 <Column field="company" header="COMPANY" />
                 <Column field="orderValue" header="ORDER VALUE" />
                 <Column field="orderDate" header="ORDER DATE" />
-                <Column header="STATUS" body={statusTemplate} />
+                <Column header="STATUS" body={statusTemplate} className='flex items-center justify-center'/>
                 <Column body={actionTemplate} style={{ width: '3rem' }} />
             </DataTable>
         </div>
